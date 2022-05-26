93057ad3ce0cfa2125fcace6b74b48e531c6cacb
Desertniy <76880716+Desertniy@users.noreply.github.com>
2022-05-26 13:10:21 +0300

    Laba3(BP)

---
+++ Laba3/Laba3(Decryption)/Laba3(Decryption).cpp
@@ -0,0 +1,24 @@
+﻿// Laba3(Decryption).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int x = 0;
+	cout << "Введите упакованную структуру:";
+	cin >> hex >> x;
+	int D = 0;
+	int M = 0;
+	int Y = 0;
+	D = (x >> 11) & 0x1F;
+	cout << dec << "D =" << D << endl;
+	M = (x >> 6) & 0x1F;
+	cout << dec << "M =" << M << endl;
+	Y = x & 0x3F;
+	cout << dec << "Y =" << Y << endl;
+	return 0;
+}
+
---
+++ Laba3/Laba3/Laba3.cpp
@@ -0,0 +1,55 @@
+﻿// Laba3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int check() {
+    int num;
+    cout << "Введите число:";
+    cin >> num;
+    while (cin.fail() || (cin.get() != '\n')){
+        cin.clear();
+        cin.ignore(cin.rdbuf()->in_avail());
+        // Вывод ошибки
+        cerr << "Ошибка ввода!!!" << endl;
+        // Повторный ввод
+        cout << "Введите значение повторно" << endl;
+        cin >> num;
+    }
+    return num;
+
+}
+
+void input_num(int &D, int &M, int &Y) {
+    while ((D < 0) || (D > 31)) {
+        cout << "Введите D (оно должно быть положительным и занимать 5 бит)" << endl;
+        D = check();
+    }
+    while ((M < 0) || (M > 31)) {
+        cout << "Введите M (оно должно быть положительным и занимать 5 бит)" << endl;
+        M = check();
+    }
+    while ((Y < 0) || (Y > 63)) {
+        cout << "Введите Y (оно должно быть положительным и занимать 6 бит)" << endl;
+        Y = check();
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int D, M, Y, x;
+    D = -1;
+    M = -1;
+    Y = -1;
+    input_num(D, M, Y);
+    x = Y;
+    x = M << 6 | x;
+    x = D << 11 | x;
+    cout << hex << x;
+    return 0;
+
+}
+
