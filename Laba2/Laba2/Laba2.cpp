ce13897f07007c6daf2e4ca727b23ce2e680d3da
Desertniy <76880716+Desertniy@users.noreply.github.com>
2022-05-26 13:08:37 +0300

    Laba2 (BP)

---
+++ Laba2/Laba2/Laba2.cpp
@@ -0,0 +1,24 @@
+﻿// Laba2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    double x, y;
+    while (true) {
+        cout << "Введите значения x и y:" << endl;
+        cin >> x >> y;
+        if ((y >= 0 && y <= 1 && x <= 1 && x >= -1) || (y <= 0 && y >= -1 && x <= 1 && x >= 0)) {
+            cout << "Точка (" << x << " ; " << y << ") входит в область" << endl;
+            break;
+        }
+        else
+            cout << "Косяк! Точка (" << x << ";" << y << ") не входит в область" << endl;
+    }
+
+    return 0;
+}
